9860518cca73b71be7ffa299a6a348b0756451a0
Kamil <78850014+Stunnerek@users.noreply.github.com>
2022-11-30 11:59:01 +0100

    wreszcie coś wyszło
    Postaram się jeszcze poprawić wizualnie ten kalkulatorek

--- kalkulator.py
+++ kalkulator.py
@@ -19,30 +19,47 @@ textbox1.place(relx=0.2, rely=0.290, anchor=tk.CENTER, width=256, height= 50)
 opcje = ['+','-','*','/']
 textbox2 = tk.Entry(root, textvariable=zmienna2 , font=('Times', 24) )
 textbox2.place(relx=0.8, rely=0.290, anchor=tk.CENTER, width=256, height= 50)
-zmienna3 =tk.StringVar()
-def Dodaj():
-    sumation = zmienna1.get() + zmienna2.get()
-    message3.config(text="Oto liczba twoja " + str(sumation))
-def Odejmij():
-    sumation = zmienna1.get() - zmienna2.get()
-    message3.config(text="Oto liczba twoja " + str(sumation))
-def Pomnoz():
-    sumation = zmienna1.get() * zmienna2.get()
-    message3.config(text="Oto liczba twoja " + str(sumation))
-def Podziel():
-    sumation = zmienna1.get() / zmienna2.get()
-    message3.config(text="Oto liczba twoja " + str(sumation))
+
+zmienna3 =tk.StringVar(root)
 def display_selected(choice):
     choice = zmienna3.get()
     print(choice)
 
-dropdown = opcja = tk.OptionMenu(root, zmienna3, *opcje, command=display_selected)
+dropdown = tk.OptionMenu(root, zmienna3, *opcje, command=display_selected)
+dropdown.pack()
 dropdown.grid(row=5,column=4)
 dropdown.place(relx=0.5, rely=0.300, anchor=tk.CENTER, width=256, height= 50)
+dropdown.config(font=('Times', 24))
 message3 = tk.Label(root,   fg= 'black', font=("bold", 30, "italic"))
 message3.grid(row=1 )
 message3.place(relx=0.5, rely=0.530, anchor=tk.CENTER)
-Przycisk = tk.Button(root, text=("Dodaj"), command=Dodaj , font=('Times', 24))
+def print_answers():
+    message3.config(text=format(zmienna3.get()))
+    return None
+
+def wybrana_metoda():
+    if zmienna3.get() == "+":
+     sumation = zmienna1.get() + zmienna2.get()
+     message3.config(text="Oto liczba twoja " + str(sumation))
+    elif zmienna3.get() == "-":
+     sumation = zmienna1.get() - zmienna2.get()
+     message3.config(text="Oto liczba twoja " + str(sumation))
+    elif zmienna3.get() == "*":
+     sumation = zmienna1.get() * zmienna2.get()
+     message3.config(text="Oto liczba twoja " + str(sumation))
+    elif zmienna3.get() == "/":
+     sumation = zmienna1.get() / zmienna2.get()
+     message3.config(text="Oto liczba twoja " + str(sumation))
+
+    
+
+def display_selected(choice):
+    choice = zmienna3.get()
+    print(choice)
+def change_table():
+    zmienna3 = dropdown.get()
+    message3.config(text="Oto liczba twoja " + str(zmienna3))
+Przycisk = tk.Button(root, text=("Oblicz"), command=wybrana_metoda , font=('Times', 24))
 Przycisk.grid(row=2, column=1)
 Przycisk.place(relx=0.5, rely=0.700, anchor=tk.CENTER, width=256, height= 50)
 
